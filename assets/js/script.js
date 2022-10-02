b9b6a50c468d9bfa5057c8a953fddc9b6aee4901
BREGLER Thomas <92312804+thomixlz@users.noreply.github.com>
2022-10-02 21:21:59 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,150 @@
+let monRectangleAffichage = document.querySelector("#rectangleAffichage");
+
+let boutonExo1 = document.querySelector("#exo1");
+let monExercice1 = document.querySelector("#exercicePos1");
+
+let boutonExo2 = document.querySelector("#exo2");
+let monExercice2 = document.querySelector("#exercicePos2");
+
+let boutonExo3 = document.querySelector("#exo3");
+let monExercice3 = document.querySelector("#exercicePos3");
+
+let boutonExo4 = document.querySelector("#exo4");
+let monExercice4 = document.querySelector("#exercicePos4");
+
+let boutonExo5 = document.querySelector("#exo5");
+let monExercice5 = document.querySelector("#exercicePos5");
+
+let boutonExo6 = document.querySelector("#exo6");
+let monExercice6 = document.querySelector("#exercicePos6");
+
+let boutonExo7 = document.querySelector("#exo7");
+let monExercice7 = document.querySelector("#exercicePos7");
+
+let boutonExo8 = document.querySelector("#exo8");
+let monExercice8 = document.querySelector("#exercicePos8");
+
+//Exercice 1 //
+
+function apparaitreExo1() {
+    monExercice1.style.display = "block";
+    monExercice2.style.display = "none";
+    monExercice3.style.display = "none";
+    monExercice4.style.display = "none";
+    monExercice5.style.display = "none";
+    monExercice6.style.display = "none";
+    monExercice7.style.display = "none";
+    monExercice8.style.display = "none";
+}
+
+boutonExo1.onclick = apparaitreExo1;
+
+//Exercice 2 //
+
+function apparaitreExo2() {
+    monExercice1.style.display = "none";
+    monExercice2.style.display = "block";
+    monExercice3.style.display = "none";
+    monExercice4.style.display = "none";
+    monExercice5.style.display = "none";
+    monExercice6.style.display = "none";
+    monExercice7.style.display = "none";
+    monExercice8.style.display = "none";
+}
+
+
+boutonExo2.onclick = apparaitreExo2;
+
+//Exercice 3 //
+
+function apparaitreExo3() {
+    monExercice1.style.display = "none";
+    monExercice2.style.display = "none";
+    monExercice3.style.display = "block";
+    monExercice4.style.display = "none";
+    monExercice5.style.display = "none";
+    monExercice6.style.display = "none";
+    monExercice7.style.display = "none";
+    monExercice8.style.display = "none";
+}
+
+
+boutonExo3.onclick = apparaitreExo3;
+
+//Exercice 4 //
+
+function apparaitreExo4() {
+    monExercice1.style.display = "none";
+    monExercice2.style.display = "none";
+    monExercice3.style.display = "none";
+    monExercice4.style.display = "block";
+    monExercice5.style.display = "none";
+    monExercice6.style.display = "none";
+    monExercice7.style.display = "none";
+    monExercice8.style.display = "none";
+}
+
+boutonExo4.onclick = apparaitreExo4;
+
+//Exercice 5 //
+
+function apparaitreExo5() {
+    monExercice1.style.display = "none";
+    monExercice2.style.display = "none";
+    monExercice3.style.display = "none";
+    monExercice4.style.display = "none";
+    monExercice5.style.display = "block";
+    monExercice6.style.display = "none";
+    monExercice7.style.display = "none";
+    monExercice8.style.display = "none";
+}
+
+boutonExo5.onclick = apparaitreExo5;
+
+
+//Exercice 6 //
+
+function apparaitreExo6() {
+    monExercice1.style.display = "none";
+    monExercice2.style.display = "none";
+    monExercice3.style.display = "none";
+    monExercice4.style.display = "none";
+    monExercice5.style.display = "none";
+    monExercice6.style.display = "block";
+    monExercice7.style.display = "none";
+    monExercice8.style.display = "none";
+}
+
+boutonExo6.onclick = apparaitreExo6;
+
+
+//Exercice 7 //
+
+function apparaitreExo7() {
+    monExercice1.style.display = "none";
+    monExercice2.style.display = "none";
+    monExercice3.style.display = "none";
+    monExercice4.style.display = "none";
+    monExercice5.style.display = "none";
+    monExercice6.style.display = "none";
+    monExercice7.style.display = "block";
+    monExercice8.style.display = "none";
+}
+
+boutonExo7.onclick = apparaitreExo7;
+
+
+//Exercice 8 //
+
+function apparaitreExo8() {
+    monExercice1.style.display = "none";
+    monExercice2.style.display = "none";
+    monExercice3.style.display = "none";
+    monExercice4.style.display = "none";
+    monExercice5.style.display = "none";
+    monExercice6.style.display = "none";
+    monExercice7.style.display = "none";
+    monExercice8.style.display = "block";
+}
+
+boutonExo8.onclick = apparaitreExo8;
